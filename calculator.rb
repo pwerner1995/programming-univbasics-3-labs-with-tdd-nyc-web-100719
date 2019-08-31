e2c0f6dfc6dfed08e9bd061bcb3f851b6cdc303e
Peter Werner <pcw1995@yahoo.com>
2019-08-31 19:27:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+first_number=5
+second_number=10 
+
+sum = firs_number + second_number
+difference= firs_number - second_number
+product = firs_number * second_number
+quotient= first_number 
