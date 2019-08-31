224a6e94aabbb0f8c5d75d0db25950fb29d848c4
Peter Werner <pcw1995@yahoo.com>
2019-08-31 19:35:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 first_number=5
 second_number=10 
 
-sum = firs_number + second_number
-difference= firs_number - second_number
-product = firs_number * second_number
+sum = first_number + second_number
+difference= first_number - second_number
+product = first_number * second_number
 quotient= first_number / second_number
