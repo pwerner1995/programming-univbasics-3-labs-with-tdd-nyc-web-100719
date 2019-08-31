d827fefe572bb8fae9475245bbe44e9102740a6a
Peter Werner <pcw1995@yahoo.com>
2019-08-31 19:29:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,4 +5,4 @@ second_number=10
 sum = firs_number + second_number
 difference= firs_number - second_number
 product = firs_number * second_number
-quotient= first_number 
+quotient= first_number / second_number
